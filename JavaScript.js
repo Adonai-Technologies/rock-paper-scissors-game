d8403f77ae33187616a884657aabfb2f6c34c0ec
Heavenba7 <akuakkonadu8@gmail.com>
2023-05-19 10:30:39 +0100

    functions

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+function getComputerChoice();
+
+function playaround(playerSelection,computerSelection){
+    return "You Lose! Paper beats Rock"
+}
+    
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+
+}
+playRound(playerSelection, computerSelection)
+
